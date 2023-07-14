a133e8310341ea027ea5e0806a58faaaa3e25dbc
Cristian Ramirez <cristiandramirezm.2024@gmail.com>
2023-07-14 06:52:31 -0500

    Initial Commit

---
+++ dice-app/dice_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dice_appApp.swift
+//  dice-app
+//
+//  Created by Cristian Daniel Ramirez Marin on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct dice_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dice-appUITests/dice_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  dice_appUITestsLaunchTests.swift
+//  dice-appUITests
+//
+//  Created by Cristian Daniel Ramirez Marin on 14/07/23.
+//
+
+import XCTest
+
+final class dice_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
